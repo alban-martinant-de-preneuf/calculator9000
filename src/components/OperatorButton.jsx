44678986e5f842b659b03670cfeccdf7115b1b6a
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-10-16 11:08:29 +0200

    adding OperatorButton component

---
+++ src/components/OperatorButton.jsx
@@ -0,0 +1,9 @@
+function OperatorButton(operator) {
+    return (
+        <>
+            <button className="operator-button">{operator}</button>
+        </>
+    );
+}
+
+export default OperatorButton;
