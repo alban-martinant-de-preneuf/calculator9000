fd82398c70dcf115bf0d26c496437628d66ab0e4
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-10-16 11:09:33 +0200

    adding BeatifulScreen component

---
+++ src/components/BeautifulScreen.jsx
@@ -0,0 +1,10 @@
+function BeautifulScreen() {
+    return (
+        <div className="screen">
+            <div className="current_calcul">0</div>
+            <div className="result">0</div>
+        </div>
+    );
+}
+
+export default BeautifulScreen;
