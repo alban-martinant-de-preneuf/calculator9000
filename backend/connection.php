356e5ec567fea71ae9a431afe30beef3b92ca001
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-10-17 13:39:07 +0200

    connection done

---
+++ backend/connection.php
@@ -0,0 +1,108 @@
+<?php
+
+session_name('calculator9000');
+session_start();
+
+$db = getDatabase();
+
+function getDatabase(): PDO {
+    try {
+        $db_config = parse_ini_file('db.ini');
+        $db = new PDO(
+            $db_config['type'] . ':dbname=' . $db_config['name']
+                . ';host=' . $db_config['host']
+                . ';charset=' . $db_config['charset'],
+            $db_config['user'],
+            $db_config['password']
+        );
+        $db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+    } catch(PDOException $e) {
+        echo json_encode(['error' => 'Could not connect to database.']);
+        die();
+    }
+    return $db;
+}
+
+function getUser(string $email, PDO $db): ?array {
+    $stmt = $db->prepare('SELECT * FROM user WHERE email = :email');
+    $stmt->execute(['email' => $email]);
+    if ($stmt->rowCount() === 0) {
+        return null;
+    }
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+if (isset($_GET['signin'])) {
+    if (!isset($_POST['email']) || !isset($_POST['password']) || !isset($_POST['password2'])) {
+        echo json_encode(['error' => 'Please fill in all fields.']);
+        die();
+    }
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password2 = $_POST['password2'];
+
+    foreach ($_POST as &$arg) {
+        if (empty($arg)) {
+            echo json_encode(['error' => 'Please fill in all fields.']);
+            die();
+        }
+        $arg = htmlspecialchars($arg);
+    }
+
+    if ($password !== $password2) {
+        echo json_encode(['error' => 'Passwords do not match.']);
+        die();
+    }
+
+    $user = getUser($email, $db);
+
+    if ($user) {
+        echo json_encode(['error' => 'Username already exists.']);
+        die();
+    }
+
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    $stmt = $db->prepare('INSERT INTO user (email, password) VALUES (:email, :password)');
+    $stmt->execute(['email' => $email, 'password' => $password]);
+
+    echo json_encode(['success' => 'User created.']);
+}
+
+if (isset($_GET['login'])) {
+    if (!isset($_POST['email']) || !isset($_POST['password'])) {
+        echo json_encode(['error' => 'Please fill in all fields.']);
+        die();
+    }
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    foreach ($_POST as &$arg) {
+        if (empty($arg)) {
+            echo json_encode(['error' => 'Please fill in all fields.']);
+            die();
+        }
+        $arg = htmlspecialchars($arg);
+    }
+
+    $user = getUser($email, $db);
+
+    if (!$user) {
+        echo json_encode(['error' => 'email or password is incorrect.']);
+        die();
+    }
+
+    if (!password_verify($password, $user['password'])) {
+        echo json_encode(['error' => 'email or password is incorrect.']);
+        die();
+    }
+
+    $_SESSION['user'] = $user;
+
+    echo json_encode(['success' => 'User logged in.']);
+}
+
+if (isset($_GET['logout'])) {
+    session_destroy();
+    echo json_encode(['success' => 'User logged out.']);
+}
